29c08d7ca44958b5c9241ec09cd290e7135b9d18
Michael Yaacoub <michael.yaacoub7@gmail.com>
2023-08-04 02:15:46 -0700

    added initial data format

---
+++ data.js
@@ -0,0 +1,31 @@
+const CATEGORIES = [
+  { name: "prayer", color: "#3b82f6" },
+  { name: "praise", color: "#eab308" },
+];
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "Pray for my roommate, he has a stage 4 cancer. His name is Craig",
+    source: "https://www.bible.com/",
+    category: "prayer",
+    votesPraying: 24,
+    votesHug: 9,
+    //votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Priase report - my classmate came to know the Lord last Sunday.",
+    source: "https://www.bible.com/",
+    category: "praise",
+    votesPraying: 11,
+    votesHug: 2,
+    //votesFalse: 0,
+    createdIn: 2019,
+  }
+];
+
+// LINK TO APP SAMPLE DATA: https://docs.google.com/spreadsheets/d/1eeldcA_OwP4DHYEvjG0kDe0cRys-cDPhc_E9P9G1e3I/edit#gid=0
+
+// 👍 🤯 ⛔️
