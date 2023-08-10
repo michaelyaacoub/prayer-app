ad8a0900d93c8a1f7b5ee3b7c2111294b8951e1d
Michael Yaacoub <michael.yaacoub7@gmail.com>
2023-08-09 17:45:07 -0700

    added functionality to form btn to hide and show form

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('Hi mom');
+console.log("Hi dad");
+
+const activate_btn = document.querySelector(".activate-btn");
+const form = document.querySelector(".fill-form");
+
+activate_btn.addEventListener("click", function () {
+    if (form.classList.contains("hide-form")) {
+        form.classList.remove("hide-form");
+        activate_btn.textContent = "Close";
+        console.log('Form opened!')
+    }
+    else {
+        form.classList.add("hide-form");
+        console.log("Form closed!");
+        activate_btn.textContent = "Share A Prayer";
+    }
+});
