04d96fc70a48568ede37a53a9e890d03a5f2acb0
Michael Yaacoub <michael.yaacoub7@gmail.com>
2023-08-14 13:56:57 -0700

    added objects and DOM manipulation

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
 console.log('Hi mom');
 console.log("Hi dad");
 
-// consts
+
+const dataRequests = [
+    {
+        user_name: "Michael",
+        date: "08-07-2023",
+        request_type: "Prayer Request",
+        request_text: "Pray for my roommate Craig, he has stage 4 cancer and has been fighting cancer for the past 2 years. Just pray that God may be glorfied and heal him and he goes cancer free.",
+        category: ["Cancer", "Healing", "Courage", "Peace"]
+    },
+    {
+        user_name: "Diana",
+        date: "08-11-2023",
+        request_type: "Praise report",
+        request_text: "Pray for peace and joy in her circumstances. Pray for peace and joy in her circumstances. Pray for peace and joy in her circumstances.",
+        category: ["Peace", "Joy"]
+    },
+    {
+        user_name: "Ryan",
+        date: "08-12-2023",
+        request_type: "Prayer Request",
+        request_text: "My mom's cousin died on the 4th of July, and her mom has been a huge support. Her mom’s cousin’s parents are still alive, so pray for everyone involved and potential drama.",
+        category: ["Peace", "Support", "Courage"]
+    }
+];
+
+
+// DOM elements.
 const activateBtn = document.querySelector(".activate-btn");
 const form = document.querySelector(".fill-form");
 const postForm = document.getElementById("submit-post");
 const msgBtn = document.querySelector(".pop-up");
+const requestList = document.querySelector(".request-list");
+
+// Creating DOM element to render requests in the list.
+requestList.innerHTML = ""
+
 
 // Show/hide form.
 activateBtn.addEventListener("click", () => {
@@ -36,3 +67,30 @@ msgBtn.addEventListener("click", () => {
     });
 });
 
+
+createPostRquestList(dataRequests);
+
+function createPostRquestList(dataArray) {
+    const htmlContent = dataArray.map(
+        (request) =>
+            `<li class="box">
+                <div class="post-details">
+                    <div style="font-family: Sono;">
+                        <p><span>From: </span>${request.user_name}</p>
+                        <p><span>Posted on: </span>${request.date}</p>
+                    </div>
+                    <p><span class="hashtag">#</span>${request.request_type}</p>
+                </div>
+                <p>
+                    ${request.request_text}
+                </p>
+                <div class="prayer-tags">
+                    <span class="tag">${request.category}</span>
+                </div>
+            </li>
+            `);
+
+    const htmlElments = htmlContent.join("");
+    requestList.insertAdjacentHTML("afterbegin", htmlElments);
+
+};
