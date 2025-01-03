6e0d6c0397b05af8bdb2343695a5fafdcb387f5c
Michael <michael.yaacoub7@gmail.com>
2025-01-02 21:45:37 -0800

    udated bg color

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,9 @@
+import logo from '../assists/logo.png'
 function Header({ showForm, setShowForm }) {
     return (
       <header className="header">
         <div className="logo">
-          <a href="https://michaelyaacoub.github.io/prayer-app/"><img src="./logo.png" alt="app logo!" /></a>
+          <a href="https://michaelyaacoub.github.io/prayer-app/"><img src={logo} alt="app logo!" /></a>
           <h1>Prayer Requests</h1>
         </div>
         <button className="btn btn-large activate-btn"
